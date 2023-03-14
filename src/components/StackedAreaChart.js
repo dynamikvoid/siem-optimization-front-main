d49edc888962df5369372689c4d62487731c11c2
UdayReddy96 <122855703+UdayReddy96@users.noreply.github.com>
2023-03-14 15:29:44 +0530

    updated stacked area

--- src/components/StackedAreaChart.js
+++ src/components/StackedAreaChart.js
@@ -1,57 +1,19 @@
-import { index, precisionRound, sum } from "d3";
 import React from "react";
-import { useState, useEffect, useCallback } from "react";
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
+import { useState, useEffect } from "react";
+import { AreaChart, Area, XAxis, YAxis, Tooltip, Legend } from "recharts";
 import moment from "moment";
 
-let data1 = [];
+let mappedData = [];
 let filterData = [];
 
-const StackedAreaChart = () => {
-  const [excelData, setData] = useState();
+const StackedAreaChart = (prop) => {
+  console.log("prop", prop.value);
   const [srcKey, setSrcKey] = useState([]);
   useEffect(() => {
     const makeRequest = async () => {
       try {
         let response = await fetch("/api/ingvoldata");
         let json = await response.json();
-
-        //global.timeStamp = json.map((x) => moment(x.date).format("YYYY-MM-DD"));
-
-        // global.sourceSystem = json.map((x) => x.sourcesystem);
-        // global.dataSizeazure = json.map((v) =>
-        //   v.sourcesystem === "Azure AD" && v.sourceSystem !== null
-        //     ? v
-        //     : v.daily_datasize_gb
-        // );
-        // global.dataSizelinux = json.map((z) =>
-        //   z.sourcesystem === "Linux" && z.sourceSystem !== null
-        //     ? z
-        //     : z.daily_datasize_gb
-        // );
-        // global.dataSizeopsmanager = json.map((y) =>
-        //   y.sourcesystem === "OpsManager" && y.sourceSystem !== null
-        //     ? y
-        //     : y.daily_datasize_gb
-        // );
-        // global.totalSizeazure = Math.round(sum(global.dataSizeazure));
-        // global.totalSizelinux = Math.round(sum(global.dataSizelinux));
-        // global.totalSizeopsmanager = Math.round(sum(global.dataSizeopsmanager));
-
-        // global.newAzure = json
-        //   .filter(
-        //     (azure) =>
-        //       azure.sourcesystem === "Azure" && azure.sourceSystem !== null
-        //   )
-        //   .map((az) => az.daily_datasize_gb);
 
         FilterData(json);
       } catch (error) {
@@ -62,7 +24,7 @@ const StackedAreaChart = () => {
   }, []);
 
   const FilterData = (json) => {
-    data1 = [];
+    mappedData = [];
     global.rand = json.length;
     global.i = 0;
 
@@ -91,11 +53,15 @@ const StackedAreaChart = () => {
     }
 
     setSrcKey(keyArray);
-    data1 = Array.from(map.values());
-    filterData = data1.slice(0, 10);
+    mappedData = Array.from(map.values());
+    filterData = mappedData.slice(0, 10);
     //setData(data1);
   };
   //console.log("setData data", data);
+
+  const valueRange = (pld) => {
+    return pld.value.sort((a, b) => b - a).slice(0, 2);
+  };
 
   const CustomTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
@@ -103,9 +69,12 @@ const StackedAreaChart = () => {
         <div className="custom-tooltip">
           <p className="label">{`${label} : `}</p>
           <div>
-            {payload.map((pld) => (
-              <div style={{ display: "inline-block", padding: 30, border: 10 }}>
-                <div style={{ color: pld.fill }}>{pld.value}</div>
+            {payload.map((pld, index) => (
+              <div
+                key={index}
+                style={{ display: "inline-block", padding: 30, border: 10 }}
+              >
+                <div style={{ color: pld.fill }}>{valueRange(pld)}</div>
                 <div style={{ color: pld.fill }}>{pld.dataKey}</div>
               </div>
             ))}
@@ -119,11 +88,11 @@ const StackedAreaChart = () => {
 
   return (
     <>
-      {data1.length > 0 && srcKey.length > 0 && (
+      {mappedData.length > 0 && srcKey.length > 0 && (
         <AreaChart
           width={870}
           height={350}
-          data={data1}
+          data={mappedData}
           margin={{
             top: 20,
             right: 40,
